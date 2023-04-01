1abdbb614ffe20a4c33743bc6dc00b12ca4cbe0e
Supra Codes <dev@supra.codes>
2023-04-01 04:56:11 +0000

    add docker and docker compose

--- setup.sh
+++ setup.sh
@@ -1,83 +1,115 @@
-#!/bin/bash
+#!/bin/zsh
 
 # 1. Update and upgrade packages
 sudo apt update -y
 sudo apt upgrade -y
 
-# 2. Install zsh if not already installed
+# 2. Cache apt update
+if [ ! -f /var/cache/apt/pkgcache.bin ]; then
+  sudo apt update -y
+else
+  echo "apt update is already cached"
+fi
+
+# 3. Install zsh if not already installed
 if [ -z "$(command -v zsh)" ]; then
   sudo apt install zsh -y
 else
   echo "zsh is already installed"
 fi
 
-# 3. Install Oh My Zsh if not already installed
+# 4. Install Oh My Zsh if not already installed
 if [ ! -d ~/.oh-my-zsh ]; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 else
   echo "Oh My Zsh is already installed"
 fi
 
-# 4. Add plugins
+# Add the ondrej/php repository
+sudo apt-get update
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository -y ppa:ondrej/php
+sudo apt-get update
+
+# Install PHP and plugins
+sudo apt-get install -y php8.2 php8.2-cli php8.2-common php8.2-json php8.2-opcache php8.2-mysql php8.2-mbstring php8.2-zip php8.2-fpm
+echo "PHP 8.2 and its plugins installed successfully."
+
+# check if composer is already installed
+if ! command -v composer &> /dev/null
+then
+    # download and install composer globally
+    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+    php composer-setup.php
+    php -r "unlink('composer-setup.php');"
+    sudo mv /usr/bin/composer
+    echo "Composer installed successfully"
+else
+    echo "Composer is already installed"
+fi
+
+# 5. Add plugins
 # a. git
 if grep -q "plugins=(git composer zsh-syntax-highlighting zsh-autosuggestions zsh-completions)" ~/.zshrc; then
   echo "git plugin is already added"
 else
-  sed -i 's/plugins=(git)/plugins=(git composer zsh-syntax-highlighting zsh-autosuggestions zsh-completions)/' ~/.zshrc
+  echo "Adding git plugin..."
+  sed -i 's/plugins=(/plugins=(git /' ~/.zshrc
 fi
 
 # b. composer
-if [ ! -f ~/.zsh/composer.zsh ]; then
-  curl https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/composer/composer.plugin.zsh --output ~/.zsh/composer.zsh
+if grep -q "plugins=(git composer zsh-syntax-highlighting zsh-autosuggestions zsh-completions)" ~/.zshrc; then
+  echo "composer plugin is already added"
 else
-  echo "composer plugin is already installed"
+  echo "Adding composer plugin..."
+  sed -i 's/plugins=(/plugins=(composer /' ~/.zshrc
 fi
 
 # c. zsh-syntax-highlighting
-if [ ! -d ~/.zsh/zsh-syntax-highlighting ]; then
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
-else
+if [ -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then
   echo "zsh-syntax-highlighting plugin is already installed"
+else
+  echo "Installing zsh-syntax-highlighting plugin..."
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+  echo "Adding zsh-syntax-highlighting plugin..."
+  sed -i 's/plugins=(/plugins=(zsh-syntax-highlighting /' ~/.zshrc
 fi
 
 # d. zsh-autosuggestions
-if [ ! -d ~/.zsh/zsh-autosuggestions ]; then
-  git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.zsh/zsh-autosuggestions
-else
+if [ -d ~/.oh-my-zsh/custom/lugins/zsh-autosuggestions ]; then
   echo "zsh-autosuggestions plugin is already installed"
+else
+  echo "Installing zsh-autosuggestions plugin..."
+  git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+  echo "Adding zsh-autosuggestions plugin..."
+  sed -i 's/plugins=(/plugins=(zsh-autosuggestions /' ~/.zshrc
 fi
 
 # e. zsh-completions
-if [ ! -d ~/.zsh/zsh-completions ]; then
-  git clone https://github.com/zsh-users/zsh-completions.git ~/.zsh/zsh-completions
-else
+if [ -d ~/.oh-my-zsh/custom/plugins/zsh-completions ]; then
   echo "zsh-completions plugin is already installed"
+else
+  echo "Installing zsh-completions plugin..."
+  git clone https://github.com/zsh-users/zsh-completions.git ~/.oh-my-zsh/custom/plugins/zsh-completions
+  echo "Adding zsh-completions plugin..."
+  sed -i 's/plugins=(/plugins=(zsh-completions /' ~/.zshrc
 fi
 
-# 5. Install Docker if not already installed
+# 6. Install Docker and Docker Compose if not already installed
 if [ -z "$(command -v docker)" ]; then
-  curl -fsSL https://get.docker.com -o get-docker.sh
-  sudo sh get-docker.sh
-  sudo usermod -aG docker $USER
-  rm -f get-docker.sh
+  echo "Installing Docker..."
+  sudo apt install docker.io -y
 else
   echo "Docker is already installed"
 fi
 
-# 6. Add PPA for PHP and install PHP 8.2 with extensions
-if ! grep -q "ondrej/php" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
-  sudo add-apt-repository ppa:ondrej/php -y
-  sudo apt update -y
+if [ -z "$(command -v docker-compose)" ]; then
+  echo "Installing Docker Compose..."
+  sudo apt install docker-compose -y
 else
-  echo "PPA for PHP is already added"
+  echo "Docker Compose is already installed"
 fi
 
-if [ -z "$(command -v php8.2)" ]; then
-  sudo apt install php8.2 php8.2-common php8.2-mysql php8.2-xml php8.2-xmlrpc php8.2-curl php8.2-gd php8.2-imagick php8.2-cli php8.2-dev php8.2-imap php8.2-mbstring php8.2-opcache php8.2-soap php8.2-zip -y
-else
-  echo "PHP 8.2 is already installed"
-fi
-
-# Configure git
-git config --global user.name "Supra Codes"
-git config --global user.email "dev@supra.codes"
+# 7. Set Zsh as the default shell
+chsh -s $(which zsh)
+echo "Zsh is now the default shell. Please log out and log back in for changes to take effect."
