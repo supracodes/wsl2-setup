1d8517fadb1b305aa77bc12eb42886f0cafa7380
Supra Codes <123155774+supracodes@users.noreply.github.com>
2023-04-01 19:54:51 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,46 +10,57 @@ sudo apt-get install -y software-properties-common
 sudo add-apt-repository -y ppa:ondrej/php
 sudo apt-get update
 
-sudo apt-get install -y php8.2 php8.2-cli php8.2-common php8.2-opcache php8.2-mysql php8.2-pgsql php8.2-sqlite3 php8.2-mbstring php8.2-zip php8.2-fpm
+sudo apt-get install -y php8.2 php8.2-cli \
+  php8.2-common \
+  php8.2-bcmath \
+  php8.2-opcache \
+  php8.2-mysql \
+  php8.2-pgsql \
+  php8.2-sqlite3 \
+  php8.2-mbstring \
+  php8.2-zip \
+  php8.2-curl \
+  php8.2-dom \
+  php8.2-xml \
+  php8.2-fpm
 echo "PHP 8.2 and its plugins installed successfully."
 
-if ! command -v composer &> /dev/null
-then
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
-sudo mv composer.phar /usr/bin/composer
-echo "Composer installed successfully"
-else
-echo "Composer is already installed"
+if ! command -v composer &> /dev/null then
+    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+    php composer-setup.php
+    php -r "unlink('composer-setup.php');"
+    sudo mv composer.phar /usr/bin/composer
+    echo "Composer installed successfully"
+  else
+    echo "Composer is already installed"
 fi
 
 echo 'export PATH="$PATH:$HOME/.config/composer/vendor/bin"' >> ~/.bashrc && source ~/.bashrc
 
 if [ -z "$(command -v docker)" ]; then
-echo "Installing Docker..."
-sudo apt install docker.io -y
+  echo "Installing Docker..."
+  sudo apt install docker.io -y
 else
-echo "Docker is already installed"
+  echo "Docker is already installed"
 fi
 
 if [ -z "$(command -v docker-compose)" ]; then
-echo "Installing Docker Compose..."
-sudo apt install docker-compose -y
+  echo "Installing Docker Compose..."
+  sudo apt install docker-compose -y
 else
-echo "Docker Compose is already installed"
+  echo "Docker Compose is already installed"
 fi
 
 if grep -q "plugins=(git)" ~/.zshrc; then
-echo "git plugin is already added"
+  echo "git plugin is already added"
 else
-echo "Adding git plugin..."
+  echo "Adding git plugin..."
 fi
 
 if grep -q "plugins=(composer)" ~/.zshrc; then
-echo "composer plugin is already added"
+  echo "composer plugin is already added"
 else
-echo "Adding composer plugin..."
+  echo "Adding composer plugin..."
 fi
 
 if [ "$(echo $SHELL)" = "/usr/bin/zsh" ]; then
