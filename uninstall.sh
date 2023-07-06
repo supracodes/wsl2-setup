fb034beec40b3eb2b18e365097fa3c9925eed22d
Supra Billionaire <billionaire@supra.codes>
2023-07-06 16:18:17 -0500

    wip

--- uninstall.sh
+++ uninstall.sh
@@ -1,27 +1,47 @@
 #!/bin/bash
 
 sudo -v
+
 config=$(cat config.json)
 
-uninstall_php_and_extensions() {
+function uninstall_php_and_extensions() {
     php_version=$(echo $config | jq -r '.apt.packages.additions.php.version')
 
     installed_php=$(sudo apt list --installed | grep "php$php_version")
 
     while IFS= read -r line; do
-        sudo apt remove $line -y
+        package=$(echo "$line" | cut -d '/' -f 1)
+        sudo apt remove "$package" -y
     done <<<"$installed_php"
-
-    sudo apt remove composer -y
-    sudo rm -r ~/.config/composer
 }
 
-remove_src_files() {
-    sudo rm -r ~/.zsh ~/.zshrc ~/.oh-my-zsh
+function uninstall_composer() {
+    sudo apt remove composer -y
+
+    if [ -d ~/.config/composer ]; then
+        sudo rm -r ~/.config/composer
+    fi
+}
+
+function remove_src_files() {
+    dirs=(
+        ~/.zsh
+        ~/.zshrc
+        ~/.oh-my-zsh
+    )
+
+    for dir in "${dirs[@]}"; do
+        if [ -d "$dir" ]; then
+            sudo rm -r "$dir"
+        fi
+    done
 }
 
 #
 uninstall_php_and_extensions
+
+#
+uninstall_composer
 
 #
 remove_src_files
